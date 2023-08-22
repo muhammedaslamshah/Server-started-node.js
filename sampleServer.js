fb2b5266c99f394ed003fcd1b6beedfa82cfeba3
Muhammed aslam shah <muhammedaslamshah700@gmail.com>
2023-08-22 08:17:21 +0530

    last plus one

--- sampleServer.js
+++ sampleServer.js
@@ -2,10 +2,23 @@ var http=require('http')
 var fs=require('fs')
 http.createServer(function(req,res){
 
+    if(req==='/'){
+
     fs.readFile('samplehtml.html',function(err,data){
+    
+
+   
     res.writeHead(200,{'Content-Type':'text/html'})
     res.write(data)
     res.end()
     })
+
+}else if(req.url==='/login'){
+    res.write('login')
+    res.end()
+}else{
+    res.write('error')
+    res.end()
+}
     
-}).listen(7000)
+}).listen(7000,() =>console.log("Server started"))
